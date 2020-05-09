b0cc76ba8ab31de28ad05808e46fcf3ad3d65490
NguyenVanThinh <ducthinhlatao4s@gmail.com>
2020-05-09 18:25:31 +0700

    Bai 3

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+
 var users = [
 			{id: 1, name: 'Thinh'},
 			{id: 2, name: 'Chung'}
 ];
+
+
+
 app.get('/', (req, res)=>{
 	res.render('index');
 });
@@ -30,12 +39,18 @@ app.get('/users/search',(req, res)=>{
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	});
 	res.render('users/index',{
-		users:matchdUsers,
-		q
+		users:matchdUsers
 	});
 });
 
+app.get('/users/create',(req, res)=>{
+	res.render('users/create');
+});
 
+app.post('/users/create',(req, res)=>{
+	users.push(req.body);
+	res.redirect('/users')
+});
 
 
 
