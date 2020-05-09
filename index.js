6173fdb340899602638fc58997a802e39dc501dc
NguyenVanThinh <ducthinhlatao4s@gmail.com>
2020-05-09 14:19:57 +0700

    Bai 3

--- index.js
+++ index.js
@@ -5,24 +5,35 @@ var port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-
+var users = [
+			{id: 1, name: 'Thinh'},
+			{id: 2, name: 'Chung'}
+];
 app.get('/', (req, res)=>{
 	res.render('index');
 });
 
 
+
+
 app.get('/users',function(req, res){
 	res.render('users/index',{
-		users:[
-			{id: 1, name: 'Thinh'},
-			{id: 2, name: 'Chung'}
-		]
+		users:users
 	});
 });
 
 
 
+app.get('/users/search',(req, res)=>{
+	var q = req.query.q;
+	var matchdUsers = users.filter((user)=>{
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index',{
+		users:matchdUsers,
+		q
+	});
+});
 
 
 
