d4d4f70fb0a44071b0036d11df9ebadaf3a6f1e5
NguyenVanThinh <ducthinhlatao4s@gmail.com>
2020-05-09 13:53:58 +0700

    Bai 2

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
 app.get('/', (req, res)=>{
-	res.send('hello');
+	res.render('index');
 });
+
+
+app.get('/users',function(req, res){
+	res.render('users/index',{
+		users:[
+			{id: 1, name: 'Thinh'},
+			{id: 2, name: 'Chung'}
+		]
+	});
+});
+
+
+
+
+
+
+
+
+
 app.listen(port, ()=>{console.log('server listen on port ' + port)});
