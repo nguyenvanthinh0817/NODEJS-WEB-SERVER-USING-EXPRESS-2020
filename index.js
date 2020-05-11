7ac67352ee26638b268486af2ef851afc34cb805
NguyenVanThinh <ducthinhlatao4s@gmail.com>
2020-05-11 22:45:39 +0700

    doi ejs thanh pug. cong bai 11

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ var port = 3000;
 
 var app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 
 
